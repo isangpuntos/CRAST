c949141bc7ae424eb7503ac8dc1fc3ea4e2c5adb
isangpuntos <isang.puntos@gmail.com>
2017-07-25 21:19:54 +0800

    test
    test

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ restService.post('/testToken', function(req, res) {
             var array = Object.keys(obj);
             for (var i = 0; i < array.length; i++) {
 				console.log(array[i])
-				console.log(date)
+				console.log(JSON.stringify(date))
                 if(array[i] === date) {
                     var innerJSON = JSON.parse(JSON.stringify(obj[array[i]]))
                     var innerArr = Object.keys(innerJSON)
